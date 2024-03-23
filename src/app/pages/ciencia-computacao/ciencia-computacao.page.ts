60e07851733875dba432167c3ff76735f5b0d639
Gabriel Ravanhan <gabriel.ravanhan@outlook.com>
2024-03-22 21:14:39 -0300

    Iframe

--- src/app/pages/ciencia-computacao/ciencia-computacao.page.ts
+++ src/app/pages/ciencia-computacao/ciencia-computacao.page.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular'; 
 
 @Component({
   selector: 'app-ciencia-computacao',
   templateUrl: './ciencia-computacao.page.html',
   styleUrls: ['./ciencia-computacao.page.scss'],
 })
-export class CienciaComputacaoPage{
+export class CienciaComputacaoPage {
+
   public voltar: boolean = true;
   public titulo: string = "Ciência da Computação";
+  public email = "computacao@unisagrado.edu.br";
 
-  constructor(private navCtrl: NavController) { }
-
+  constructor() { }
 }
