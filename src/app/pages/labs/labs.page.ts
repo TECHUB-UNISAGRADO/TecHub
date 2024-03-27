4634f2be5f0e7d69df4a69f6281d851eaf349e63
samuel-fm-coding <samuelfernandescor@gmail.com>
2024-03-27 09:07:53 -0300

    inserção de imagens e logica de troca de labs

--- src/app/pages/labs/labs.page.ts
+++ src/app/pages/labs/labs.page.ts
@@ -10,34 +10,28 @@ export class LabsPage implements OnInit {
   public parametroRecebido: string = '';
   public voltar: boolean = true;
   public titulo: string = ''; // Inicialize como vazio
-  public texto: string = ''
+  public texto: string = '';
 
   constructor(private route: ActivatedRoute) {}
 
   currentSlide: number = 0;
   images: { src: string, alt: string }[] = [];
+  imageDirectory: string = 'assets/imgs/labs/';
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.parametroRecebido = params['lab']; // Recupera o parâmetro passado
       this.titulo = 'F-' + this.parametroRecebido; // Defina o título após receber o parâmetro
-      
+
+      // Define a lista de imagens com base no parâmetro recebido
       if (this.parametroRecebido === '008') {
-        this.images = [
-          { src: 'assets/imgs/cursos/jogos-digitais.jpg', alt: 'Outra Imagem 1' },
-          
-        ];
+        this.loadImagesFromDirectory('F008/');
         this.texto = "As imagens acima apresentam o ambiente e todos os equipamentos tecnológicos dispostos no laboratório F-008, incluindo computadores, componentes de rede e projetores.";
-      } else if(this.parametroRecebido == '102') {
-        this.images = [
-          { src: 'assets/imgs/cursos/ciencia-da-computacao.jpg', alt: 'Imagem 2' }
-        ];
+      } else if(this.parametroRecebido == '102'){
+        this.loadImagesFromDirectory('F102/');
         this.texto = "As imagens acima apresentam o ambiente e todos os equipamentos tecnológicos dispostos no laboratório F-102, incluindo computadores, componentes de rede e projetores.";
-      }
-      else{
-        this.images = [
-          { src: 'assets/imgs/cursos/ciencia-da-computacao.jpg', alt: 'Imagem 2' }
-        ];
+      } else{
+        this.loadImagesFromDirectory('F108/');
         this.texto = "As imagens acima apresentam o ambiente e todos os equipamentos tecnológicos dispostos no laboratório F-108, incluindo computadores, componentes de rede e projetores.";
       }
     });
@@ -45,6 +39,17 @@ export class LabsPage implements OnInit {
     setInterval(() => {
       this.moveSlide(1);
     }, 10000);
+  }
+
+  loadImagesFromDirectory(directory: string): void {
+    // Supondo que você tenha uma lista de nomes de arquivos predefinida
+    const imageNames: string[] = ['imagem1.JPG', 'imagem2.JPG', 'imagem3.JPG', 'imagem4.JPG'];
+
+    // Construa a lista de imagens com base nos nomes de arquivo e diretório
+    this.images = imageNames.map(name => ({
+      src: this.imageDirectory + directory + name,
+      alt: name,
+    }));
   }
 
   moveSlide(n: number): void {
